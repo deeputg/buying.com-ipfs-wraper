571c90fc3d447b2b2e6837ae08c631448f64229e
deeputg <deeputg1992@gmail.com>
2021-02-15 13:28:53 +0530

    fix listenig port issue

--- app.private.js
+++ app.private.js
@@ -34,7 +34,7 @@ app.get("/", async (req, res) => {
 
 
 
-let port = process.env.PORT | 3000
+let port = process.env.PORT || 3000
 
 app.listen(port, () => {
     console.log("Listening to port ", port)
